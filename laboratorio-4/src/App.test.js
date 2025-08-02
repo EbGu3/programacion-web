455b44c9c181c18ab3e70327f1adbfb0ce82e7d7
Eber Guerra <54523553+EbGu3@users.noreply.github.com>
2025-08-02 09:40:31 -0600

    feat(lab-4): Change text to sum result in view

--- laboratorio-4/src/App.test.js
+++ laboratorio-4/src/App.test.js
@@ -2,8 +2,7 @@ import { render, screen } from '@testing-library/react';
 import App from './App';
 
 test('renders learn react link', () => {
-  // Ver si se renderiza learn react
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const linkElement = screen.getByText(/4/i);
   expect(linkElement).toBeInTheDocument();
 });
